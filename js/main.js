ae8e122e4dfc538c22db61a3f1b0790d7db62c97
Helen Nazarenko <nazarenh@bc.edu>
2019-11-17 22:59:39 -0500

    edited x-axis and added tooltip

--- js/main.js
+++ js/main.js
@@ -1,24 +1,6 @@
-// Promise.all([ 
-//     d3.csv('data/mining_algorithms.csv')   
-// ]).then(data=>{ 
-//     let miningData = data
-//     window.miningData = min
-//     console.log('mining data:', miningData);
-// })
-
-
-// function getData()
-// {
-//     let dataset = []
-//     d3.csv('data/mining_algorithms.csv', function(data){
-//         console.log(data)
-//         dataset.push(data)
-//         console.log(dataset.size)
-//     })
-//     console.log(dataset)
-//     return dataset
-
-// }
+//next:
+//add transition to next vis.
+//next vis.
 
 window.createGraphic = function(graphicSelector, newdata){
 	var graphicEl = d3.select('.graphic')
@@ -66,9 +48,11 @@ window.createGraphic = function(graphicSelector, newdata){
                 .attr('transform', function(d){
                         return translate(chartSize / 2, chartSize / 2)
                 })
+            
 
             item.select('circle')
                 .style('fill','pink')
+                .attr('class','balloon')
 				.transition(t)
                 .attr('r', function(d)
                 {
@@ -91,14 +75,26 @@ window.createGraphic = function(graphicSelector, newdata){
                         return 1;
                     return 0;
                 })
+
+            d3.selectAll('.item text').remove()
+            var circleText = d3.selectAll(".item").append("text")
+                .attr("class", "circleText")	
+                .style('opacity',0)		
+                .style('fill','white')
+                .html("Coins")
+                .style('opacity',1)
 		},
 
 		function step1() {
             console.log('step  1');
+
+            let res = d3.select('.tooltip');
+            res.style('opacity',0);
+
 			var t = d3.transition()
 				.duration(600)
-				.ease(d3.easeQuadInOut)
-			
+                .ease(d3.easeQuadInOut)
+                	
 			// circles are positioned
             var item = graphicVisEl.selectAll('.item')
             
@@ -109,17 +105,19 @@ window.createGraphic = function(graphicSelector, newdata){
             .attr("class", "x-axis")
             .attr("transform", "translate(" + margin + "," + scaleSize + ")")
             .style("opacity", 1)
-			
+            
+            var parseTime = d3.timeParse("%Y");
 			item.transition(t)
 				.attr('transform', function(d, i) {
                     if (d == "Coins")
                     {
                         return translate(chartSize/2, chartSize/2 - margin)
                     }
-					return translate(scaleX(d.year), chartSize / 2 -margin - 10*i)
+					return translate(scaleX(parseTime(d.year)), chartSize / 2 -margin - 10*i)
 				})
 
             item.select('circle')
+                .attr('class','')
                 .style('fill', 'pink')
 				.transition(t)
                 .attr('r', function(d)
@@ -135,7 +133,41 @@ window.createGraphic = function(graphicSelector, newdata){
                     if (d == "Coins")
                         return 0;
                     return 1;
+                });
+
+
+                // Define the circletext:
+
+            
+            var circleText = d3.selectAll(".item").append("text")
+                .attr("class", "circleText")	
+                .style('opacity',0)		
+                .style('fill','white')
+                .html(function(d){return d.symbol;})
+                .style('opacity',1)
+
+            circleText.exit()
+                        
+
+            let circles = item.select('circle')
+                .on('mouseover', function(d){
+                    let res = d3.select('.tooltip');
+                    res.transition().duration(50)
+                        .style('opacity',1)
+                    res.html('<strong>'+d.name+'</strong>'+
+                        '<br>Algorithm: '+d.algo+'<br>Market Cap: '+d.marketcap);
+                    res.style('right', 50 + "px");
+                    res.style('top', 100 + "px");
+                    console.log(d.name)
                 })
+                .on('mouseout', function(){
+                    let res = d3.select('.tooltip');
+                    res.style('opacity',0);
+                })
+
+
+
+
 
 			item.select('text')
 				.transition(t)
@@ -175,15 +207,67 @@ window.createGraphic = function(graphicSelector, newdata){
 				.transition(t)
 				.style('fill', function(d, i) {
 					return colorScale(d.algo);
-				})
+                })
+                
+
+            // Define the circletext:
+            d3.selectAll(".item text").remove()
+
+            d3.selectAll('.item').append("text")
+            .attr("class", "circleText")				
+            .html(function(d){return d.algo;})
+            .style("opacity", 1)
+            
+
+
+
+            let circles = item.select('circle')
+            .transition(t)
+            // .delay(50)
+            .attr('r', function(d, i) {
+                return minR;
+            })
+            circles = item.selectAll('circle')
+            .on('mouseover', function(d){
+                // d3.selectAll('.tooltip').remove();
+                let res = d3.selectAll('.tooltip')
+                res.transition().duration(50)
+                    .style('opacity',1)
+                res.html('<strong>'+d.name+'</strong>'+
+                '<br>Algorithm: '+d.algo+'<br>Market Cap: '+d.marketcap);
+                // console.log((d3.event.pageX), d3.event.pageY-1400);
+                res.style('right', 50 + "px");
+                res.style('top', 100 + "px");
+                // div.style('left', (d3.event.pageX) + "px")
+                // div.style('top', (d3.event.pageY) + "px");
+                console.log(d.name)
+            })
+            .on('mouseout', function(){
+                let res = d3.select('.tooltip');
+                res.style('opacity',0);
+            })
         },
+
+
         function step3() {
             //bubbles grow in size to represent market cap.
             console.log('step  3');
 
+
+            var algos=[];
+            for (let i=0;i<newdata.length;i++){
+                if (!algos.includes(newdata[i].algo)) algos.push(newdata[i].algo);
+            }
+            console.log(algos);
+            var colorScale = d3.scaleOrdinal(d3.schemeAccent) 
+                .domain(algos)
+
+            //remove symbol titles from circles:
+            d3.selectAll('.circleText').style('opacity',0);
+
             var marketScale = d3.scaleLinear()
                 .domain(d3.extent(newdata,function(d){return d.marketcap;}) )
-                .range([20,70]);
+                .range([10,30]);
         
             var t = d3.transition()
 				.ease(d3.easeLinear)
@@ -196,6 +280,9 @@ window.createGraphic = function(graphicSelector, newdata){
 				.delay(50)
 				.attr('r', function(d, i) {
 					return marketScale(d.marketcap);
+                })
+                .style('fill', function(d, i) {
+					return colorScale(d.algo);
                 })
         },
         function step4() {
@@ -247,20 +334,18 @@ window.createGraphic = function(graphicSelector, newdata){
         console.log(lowestVal)
         // d3.min(data,d=>d.Income)
 
+        var parseTime = d3.timeParse("%Y");
         console.log(chartSize)
 		scaleX
-			.domain([lowestVal, highestVal])
+			.domain([parseTime(lowestVal), parseTime(highestVal)])
             .range([margin, size-margin]);
-			// .padding(1)
-
-		// scaleR
-		// 	.domain(extent)
-        //     .range([minR, maxR])
 
         var scaleSize = chartSize/2 + margin
+
+        
         
         console.log(scaleX)
-        xAxis = d3.axisBottom().scale(scaleX);
+        xAxis = d3.axisBottom().scale(scaleX).tickFormat(d3.timeFormat("%Y"));
         //add x-axis to group xg:
         var xg = svg.append("g")
         .call(xAxis)
