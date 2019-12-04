61c081a25612f60c07482dd7cd2aae866e53f7eb
Linda Chen <lindachenny@gmail.com>
2019-12-04 17:03:00 -0500

    fade improvements

--- js/fade.js
+++ js/fade.js
@@ -10,20 +10,63 @@ $(window).on("scroll", function () {
     var pageBottom = pageTop + $(window).height()
 
     //Threshold for sections coming into view.
-    var visibleThreshold = pageBottom - $(window).height() * 1.3
+    var visibleThreshold = pageBottom - $(window).height() * 0.1
     
     var tags = $("section")
     //Independently check each section
     for (var i = 0; i < tags.length; i++) {
         var tag = tags[i]
-    
-        if ($(tag).position().top < visibleThreshold
-            && $(tag).position().top > visibleThreshold - 800
-        ) {
+
+        var sectionHeight = $(tag.getBoundingClientRect().height)[0];
+        var sectionBottom = $(tag).position().top + sectionHeight;
+        console.log("Top tag position")
+        console.log($(tag).position().top)
+
+        //Checks to see whether the text has entered the frame of view
+        //From the bottom
+        var topOnPage = 
+        //Above bottom
+        ($(tag).position().top < visibleThreshold)
+        //Below top
+        && ($(tag).position().top > pageTop + 50)
+
+        //Checks to see whether the text is still in view
+        //From the top
+        var bottomOnPage = false
+        //Above bottom
+        // (sectionBottom < visibleThreshold)
+        // //Below top
+        // && (sectionBottom > pageTop + 50)
+
+        var textInView = topOnPage || bottomOnPage;
+        if (textInView) {
             $(tag).addClass("visible")
+            console.log("Top tag position")
+            console.log($(tag).position().top)
+            console.log("Section height")
+            console.log(sectionHeight)
+            console.log("Section bottom")
+            console.log(sectionBottom)
+            console.log("Page Top")
+            console.log(pageTop)
         } else {
             $(tag).removeClass("visible")
         }
+    
+        // //If the top of the text peeks over the bottom of the screen
+        // Oh dear this is a disaster
+        // if (
+        //     ($(tag).position().top < visibleThreshold)
+        // //OR the bottom of the text is still on the screen
+        //     || ($(tag).position().top + $(tag).clientHeight < visibleThreshold - 100)
+        //     && 
+        // //AND 
+        //     ($(tag).position().top > pageTop + 100)
+        // ) {
+        //     $(tag).addClass("visible")
+        // } else {
+        //     $(tag).removeClass("visible")
+        // }
     }
 })
 
