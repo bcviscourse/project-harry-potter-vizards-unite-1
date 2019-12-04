47420c103386ea9ae679d3c73050d3257a6bb30a
Linda Chen <lindachenny@gmail.com>
2019-12-04 17:36:52 -0500

    fade working again

--- js/fade.js
+++ js/fade.js
@@ -1,4 +1,34 @@
 
+// Where element is the DOM element you'd like to test for visibility
+function isHidden(element) {
+    return (element.offsetParent === null)
+}
+
+//IS VISIBLE
+function isVisible(elem) {
+    if (!(elem instanceof Element)) throw Error('DomUtil: elem is not an element.');
+    const style = getComputedStyle(elem);
+    if (style.display === 'none') return false;
+    if (style.visibility !== 'visible') return false;
+    if (style.opacity < 0.1) return false;
+    if (elem.offsetWidth + elem.offsetHeight + elem.getBoundingClientRect().height +
+        elem.getBoundingClientRect().width === 0) {
+        return false;
+    }
+    const elemCenter   = {
+        x: elem.getBoundingClientRect().left + elem.offsetWidth / 2,
+        y: elem.getBoundingClientRect().top + elem.offsetHeight / 2
+    };
+    if (elemCenter.x < 0) return false;
+    if (elemCenter.x > (document.documentElement.clientWidth || window.innerWidth)) return false;
+    if (elemCenter.y < 0) return false;
+    if (elemCenter.y > (document.documentElement.clientHeight || window.innerHeight)) return false;
+    let pointContainer = document.elementFromPoint(elemCenter.x, elemCenter.y);
+    do {
+        if (pointContainer === elem) return true;
+    } while (pointContainer = pointContainer.parentNode);
+    return false;
+}
 
 $(window).on("scroll", function () {
     //How far you've scrolled
@@ -41,14 +71,14 @@ $(window).on("scroll", function () {
         var textInView = topOnPage || bottomOnPage;
         if (textInView) {
             $(tag).addClass("visible")
-            console.log("Top tag position")
-            console.log($(tag).position().top)
-            console.log("Section height")
-            console.log(sectionHeight)
-            console.log("Section bottom")
-            console.log(sectionBottom)
-            console.log("Page Top")
-            console.log(pageTop)
+            // console.log("Top tag position")
+            // console.log($(tag).position().top)
+            // console.log("Section height")
+            // console.log(sectionHeight)
+            // console.log("Section bottom")
+            // console.log(sectionBottom)
+            // console.log("Page Top")
+            // console.log(pageTop)
         } else {
             $(tag).removeClass("visible")
         }
@@ -67,6 +97,20 @@ $(window).on("scroll", function () {
         // } else {
         //     $(tag).removeClass("visible")
         // }
+    }
+
+    //This works better. Used for the scrollytelling points.
+    var storyTags = $("section")
+    //Independently check each section
+    for (var i = 0; i < tags.length; i++) {
+        var tag = storyTags[i]
+
+        if (!isVisible(tag)){
+            $(tag).addClass("visible")
+        } else {
+            $(tag).removeClass("visible")
+        }
+
     }
 })
 
